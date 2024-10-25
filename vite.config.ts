b5778b24139ecdddfe07c96ea7855c6dfc780a8a
sgksm85 <sgksm85@gmail.com>
2024-10-25 21:58:43 +0900

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,23 @@ import dns from "dns";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/server-options.html#server-host
 dns.setDefaultResultOrder("verbatim");
 
-// make sure vite picks up all html files in root, needed for vite build
 const allHtmlEntries = fs
   .readdirSync(".")
   .filter((file) => path.extname(file) === ".html")
   .reduce((acc: Record<string, string>, file) => {
     acc[path.basename(file, ".html")] = path.resolve(__dirname, file);
-
     return acc;
   }, {});
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // これを追加
   build: {
     rollupOptions: {
       input: allHtmlEntries,
     },
+    outDir: "dist", // これを追加
   },
   plugins: [react()],
   server: {
